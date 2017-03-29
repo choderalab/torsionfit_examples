9f78a1e280b2e463d6b3de7d3c077c30b9e4f866
Chaya Stern <chaya.stern@choderalab.org>
2017-03-29 13:10:32 -0400

    randomize starting condition and run many times

---
+++ butane/sampling_torsions/c_c_c_c_torsion/n5_rj/setup_and_run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Parameters for toy model
+RJ=True
+DB=random
+
+SAMPLES='100000'
+
+REPEATS=(20 21 22 23 24 25 26 27 28 29)
+
+mkdir $SAMPLES
+cd $SAMPLES
+
+for C in ${REPEATS[*]}; do
+    mkdir ${DB}_${SAMPLES}_${C}
+    cd ${DB}_${SAMPLES}_${C}
+    DBNAME=${DB}_${SAMPLES}_${C}
+    sed "s/REPLACE/-rj $RJ -d ${DBNAME}.sqlite -i $SAMPLES/" ../../submit_dummy > submit
+    python ../../run_sampler.py -rj $RJ -d ${DBNAME}.sqlite
+    cd ../
+done
