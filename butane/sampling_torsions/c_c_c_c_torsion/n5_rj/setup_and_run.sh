631a21a863688d4a70f7d838f78a7838dc22c9bb
Chaya Stern <chaya.stern@choderalab.org>
2017-03-29 21:40:00 -0400

    starting from end of rj

--- butane/sampling_torsions/c_c_c_c_torsion/n5_rj/setup_and_run.sh
+++ butane/sampling_torsions/c_c_c_c_torsion/n5_rj/setup_and_run.sh
@@ -2,20 +2,20 @@
 
 # Parameters for toy model
 RJ=True
-DB=random
+DB=from_rj
 
-SAMPLES='100000'
+SAMPLES='10000'
 
-REPEATS=(20 21 22 23 24 25 26 27 28 29)
+REPEATS=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24)
 
-mkdir $SAMPLES
-cd $SAMPLES
+mkdir from_rj_${SAMPLES}
+cd from_rj_${SAMPLES}
 
 for C in ${REPEATS[*]}; do
     mkdir ${DB}_${SAMPLES}_${C}
     cd ${DB}_${SAMPLES}_${C}
     DBNAME=${DB}_${SAMPLES}_${C}
-    sed "s/REPLACE/-rj $RJ -d ${DBNAME}.sqlite -i $SAMPLES/" ../../submit_dummy > submit
+    sed "s/REPLACE/-rj $RJ -d ${DBNAME}.sqlite -i $SAMPLES -r $C/" ../../submit_dummy > submit
     qsub submit
     cd ../
 done
