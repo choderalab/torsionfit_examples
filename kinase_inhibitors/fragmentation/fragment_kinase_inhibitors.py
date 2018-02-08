62de52a7ae2114ce2cd82a312ecfef2ba4510247
ChayaSt <chaya.stern@choderalab.org>
2018-02-08 18:22:53 -0500

    updated example of generating fragments for kinase inhibitors

--- kinase_inhibitors/fragmentation/fragment_kinase_inhibitors.py
+++ kinase_inhibitors/fragmentation/fragment_kinase_inhibitors.py
@@ -1,15 +1,18 @@
-from openeye import oechem, oedepict
 import pandas as pd
-from openmoltools.openeye import get_charges, smiles_to_oemol
-from torsionfit.qmscan import fragment
-
+from torsionfit.qmscan import fragment, utils
 
 # Import FDA approved kinase inhibitors (As of Jan 2018)
 kinase_inhibitors = pd.read_csv('../clinical-kinase-inhibitors.csv')
 
+# Generate smi input file
+smiles = []
 for i, inhibitor in kinase_inhibitors.iterrows():
-    print('Fragmenting {}'.format(inhibitor['inhibitor']))
-    mol = smiles_to_oemol(inhibitor['smiles'])
-    charged, frags = fragment.generate_fragments(mol)
-    oname = '{}.pdf'.format(inhibitor['inhibitor'])
-    fragment.ToPdf(charged, oname, frags)
+    smiles.append("{} {}".format(inhibitor['smiles'], inhibitor['inhibitor']))
+
+# Write out file
+out_dir = '/Users/chayastern/src/ChayaSt/torsionfit_examples/kinase_inhibitors/fragmentation/'
+base_fname = 'kinase_inhibitors'
+input_smi = utils.to_smi(smiles, out_dir, base_fname, return_fname=True)
+
+# Generate nrotor_N.smi files of fragments. Have the option to generate pdf file of fragments for visualization.
+fragment.generate_fragments(input_smi, out_dir, pdf=True)
